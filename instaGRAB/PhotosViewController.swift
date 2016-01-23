8f365283681ee9166e8290801fdf3e21ac95137c
Dylan James SMith <dsmith95@mail.ccsf.edu>
2016-01-22 21:53:14 -0800

    create github repo
    completed milestone 2 and publish

---
+++ instaGRAB/PhotosViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PhotosViewController.swift
+//  instaGRAB
+//
+//  Created by Dylan Smith on 1/22/16.
+//  Copyright © 2016 dylan. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotosViewController: UIViewController {
+
+    var igAPI: [NSDictionary]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let clientId = "Put your client id here"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.igAPI = responseDictionary["results"] as? [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
